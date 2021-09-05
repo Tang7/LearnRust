0e32e794dacdec283aa1dbcdf2019b97c181172d
T7 <still.tangqi@gmail.com>
2021-09-05 22:39:23 +0300

    Initial CLI using Clap
    Study Trait and using clap trait

--- httpie/README.md
+++ httpie/README.md
@@ -1,3 +1,22 @@
 ## HTTPie
 
 HTTPie in Rust.
+
+### Crate Management
+1. Install _cargo-edit_: `cargo install cargo-edit`
+2. Add needed package, i.e, `cargo add clap --allow-prerelease`
+
+
+### Command Line Process
+
+**clap**
+
+Key Studies:
+* Using trait `#[]`
+* Why needs to derive trait in the remaining types?
+* What does `Parse()` do?
+* What is `{:?}`?
+
+### HTTP Process
+
+### Style Output
--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,3 +1,32 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap, Debug)]
+#[clap(version = "1.0")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: Subcommand,
+}
+
+// Only support Get / Post for now
+#[derive(Clap, Debug)]
+enum Subcommand {
+    Get(Get),
+    Post(Post),
+}
+
+#[derive(Clap, Debug)]
+struct Get {
+    url: String, // http request url
+}
+
+#[derive(Clap, Debug)]
+struct Post {
+    url: String, // http request url
+    body: Vec<String>, // http request data, json Key-Value pair format
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
 }
