592b0a05ba5b95ad89113339f8e1dbfcc949d972
T7 <still.tangqi@gmail.com>
2021-09-12 15:46:30 +0300

    Add Key-Value pair filter
    Create new struct `KvPair` as the container for the parsed (Key,Value)
    pair.
    
    Implement `FromStr` trait for `KvPair`, to restrict the `KvPair` input format
    to be k=v. So that each `KvPair` can be easily verified and obtained after
    calling `parse()` function.

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,6 +1,7 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::{AppSettings, Clap};
 use reqwest::Url;
+use std::str::FromStr;
 
 #[derive(Clap, Debug)]
 #[clap(version = "1.0")]
@@ -26,14 +27,39 @@ struct Get {
 #[derive(Clap, Debug)]
 struct Post {
     #[clap(parse(try_from_str = parse_url))]
-    url: String,       // http request url
-    body: Vec<String>, // http request data, json Key-Value pair format
+    url: String, // http request url
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>, // http request data, json Key-Value pair format
 }
 
 fn parse_url(s: &str) -> Result<String> {
     let _url: Url = s.parse()?;
 
     Ok(s.into())
+}
+
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split_iter = s.split("=");
+        // define a closure returns error if the format is incorrect.
+        let err = || anyhow!(format!("Failed to parse KvPair {}", s));
+        Ok(Self {
+            k: (split_iter.next().ok_or_else(err)?).to_string(),
+            v: (split_iter.next().ok_or_else(err)?).to_string(),
+        })
+    }
 }
 
 fn main() {
