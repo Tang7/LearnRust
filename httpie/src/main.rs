e587264c5060ce5997c997f1f5ddfc887553a437
Tang7 <still.tangqi@gmail.com>
2022-01-29 23:17:36 +0200

    add parse_url and parse_kv_pair unit test

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -40,7 +40,7 @@ fn parse_url(s: &str) -> Result<String> {
     Ok(s.into())
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct KvPair {
     k: String,
     v: String,
@@ -67,4 +67,36 @@ impl FromStr for KvPair {
 fn main() {
     let opts: Opts = Opts::parse();
     println!("{:?}", opts);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into()
+            }
+        );
+    }
 }
